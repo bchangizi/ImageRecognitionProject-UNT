5588d8116addad85d11955c8ddf325e1d3e35e3f
Brian Lee <brian@the-lee-home.us>
2013-02-20 15:11:46 -0600

    Convert to c++ api (previously mixing C and C++ apis)

--- selector/selector.cpp
+++ selector/selector.cpp
@@ -35,18 +35,21 @@ bool dragging = false;
  */
 bool finished = false;
 /*
+ * Set to false when the GUI needs to be closed
+ */
+bool running = true;
+/*
+ * When true, show the help menu.
+ */
+bool help = false;
+/*
  * Contains coordinates for the selected image
  */
-CvRect selection;
+Rect selection;
 /*
  * Called when the named window gets a mouse event
  */
 void mouse_callback(int event, int x, int y, int flags, void* param);
-/*
- * Compute the image chunk to show
- * Set 'finished' to true if we are done dragging
- */
-void show_selection(IplImage* current_image);
 
 /*
  * Entry point
@@ -54,27 +57,30 @@ void show_selection(IplImage* current_image);
 int main(int argc, char *argv[]) {
 
 	const char* filename = "lenaN.tif";
-	IplImage* img;
-	IplImage* img_selection;
-	IplImage* backup;
+	Mat img, img_selection, backup;
 
 	/*
 	 * Read the image 
 	 * Copy to a backup
 	 */
-	img = cvLoadImage(filename);
-	backup = cvLoadImage(filename);
-	img_selection = cvCloneImage(img);
+	img = imread(filename, CV_LOAD_IMAGE_COLOR);
+	img_selection = img.clone();
+	backup = imread(filename, CV_LOAD_IMAGE_COLOR);
 
-	cvNamedWindow(filename, CV_WINDOW_AUTOSIZE);
-	cvSetMouseCallback(filename, mouse_callback, (void*) img);
+	namedWindow(filename, CV_WINDOW_AUTOSIZE);
+	setMouseCallback(filename, mouse_callback, (void*) &img);
 
-	while(1) {
-
-		cvCopyImage(img, img_selection);
+	while(running) {
+		/*
+		 * Reset the image so drawing a rectangle is animated
+		 */
+		backup.copyTo(img);
 
 		if(dragging) {
-			show_selection(img_selection);
+			/*
+			 * Draw the current mouse selection
+			 */
+			rectangle(img, selection, Scalar(0, 255, 0), 1, 8, 0);
 		}
 		else if(finished) {
 			finished = false;
@@ -84,54 +90,38 @@ int main(int argc, char *argv[]) {
 			 */
 			if(selection.width < 10 || selection.height < 10)
 				continue;
-
-			/*
-			 * Create a 'region of interest' from the selection rectangle
-			 * Then copy it and show it in a new window.
-			 */
-			IplImage* interest;
-			cvSetImageROI(backup, selection);
-			interest = cvCreateImage(cvGetSize(backup), backup->depth, backup->nChannels);
-			cvCopy(backup, interest, NULL);
-			cvResetImageROI(backup);
-
-			/* 
-			 * hack to make the selection rectangle disappear
-			 * (copy a backup into the image being drawn)
-			 *
-			 * Breaks if the window is too small (oops?)
-			 */
-			cvCopy(backup, img, NULL);
-			cvShowImage(filename, img_selection);
-
-			cvNamedWindow("Region of Interest", CV_WINDOW_AUTOSIZE);
-			cvShowImage("Region of Interest", interest);
+		
+			Mat interest = img_selection(selection);
+			imshow("Region of Interest", interest);
 		}
 
 		// show the image in a window
-		cvShowImage(filename, img_selection);
+		imshow(filename, img);
 
 		/* 
-		 * if ESCAPE is pressed, exit
-		 * wait 10 milliseconds for mouse input
+		 * wait 10 milliseconds for keyboard input
 		 */
-		if(cvWaitKey(10) == ESCAPE)
-			break;
+		int keyCode = cvWaitKey(10);
+		switch(keyCode) {
+			case ESCAPE: {
+				running = false;
+				break;
+			 }				 
+			/*
+			 * Press h for help
+			 */
+			case 'h': {
+				//show help
+				break;
+			}
+		}
 	}
-
-	/*
-	 * cleanup
-	 */
-	cvReleaseImage(&img);
-	cvReleaseImage(&img_selection);
-	cvDestroyWindow(filename);
 
 	return 0;
 }
 
 void mouse_callback(int event, int x, int y, int flags, void* param) {
-
-	IplImage* current_image = (IplImage*) param;
+	Mat* current_image = (Mat*) param;
 
 	switch(event) {
 		case CV_EVENT_MOUSEMOVE: {
@@ -154,6 +144,7 @@ void mouse_callback(int event, int x, int y, int flags, void* param) {
 			// left button up means dragging is done, 
 			// draw the selection
 			dragging = false;
+			finished = true;
 
 			// if the user dragged up or to the left
 			// we cant have a negative box, so shift
@@ -167,19 +158,7 @@ void mouse_callback(int event, int x, int y, int flags, void* param) {
 				selection.height *= -1;
 			}
 
-			show_selection(current_image);
 			break;
 		}
 	}
-}
-
-/*
- * Draw a green rectangle on the image
- * 
- * When the user stops moving the mouse, consider the selection finished.
- */
-void show_selection(IplImage* current_image) {
-	cvRectangle(current_image, cvPoint(selection.x, selection.y), cvPoint(selection.x + selection.width, selection.y + selection.height), cvScalar(0x00, 0xFF, 0x00));
-	if(!dragging)
-		finished = true;
 }

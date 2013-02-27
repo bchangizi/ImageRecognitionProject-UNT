bdea093c1d18dc628be67c45a4416e4827b8c19a
Brian Lee <brian@the-lee-home.us>
2013-02-27 15:33:01 -0600

    Change to camel case to consistancy with opencv api

--- selector/defines.h
+++ selector/defines.h
@@ -27,19 +27,19 @@ bool help = false;
  * If use_edges is true, display the edge map
  * instead of the original image.
  */
-bool use_edges = false;
+bool useEdges = false;
 
 /*
  * If reduce_noise is true, apply a median blur
  * (simple noise reduction).
  */
-bool reduce_noise = false;
+bool reduceNoise = false;
 
 /*
  * If max_contrast is true, convert the image
  * to its maximum contrast.
  */
-bool max_contrast = false;
+bool maxContrast = false;
 
 /*
  * Contains coordinates for the selected image.
@@ -49,23 +49,24 @@ Rect selection;
 /*
  * Called when the named window gets a mouse event.
  */
-void mouse_callback(int event, int x, int y, int flags, void* param);
+void mouseCallback(int event, int x, int y, int flags, void* param);
 
 /*
  * Break the image into its channels and equalize their histograms.
  */
-void maximize_contrast(Mat &img);
+void maximizeContrast(Mat &img);
 
 /*
- * Show the Sobel edge map of the image
+ * Given an image and a subimage, find the subimage
+ * and highlight it, and store the result
  */
-void detect_edges(Mat &img);
+void findSelection(Mat& source, Mat& subimage, Rect& result);
 
 /*
  * Ridiculous hack to put text on newlines...
  * Each array index is effectively a new line.
  */
-const char *help_array[] = {"ESC -- quit.",
+const char *helpArray[] = {"ESC -- quit.",
 							"h   -- toggle this menu",
 							"e   -- toggle edge map",
 							"r   -- reduce noise (median blur)",
--- selector/selector.cpp
+++ selector/selector.cpp
@@ -42,36 +42,26 @@ int main(int argc, char *argv[]) {
 	 * Create the window and add a mouse event handler
 	 */
 	namedWindow(filename, CV_WINDOW_AUTOSIZE);
-	setMouseCallback(filename, mouse_callback, (void*) &img);
+	setMouseCallback(filename, mouseCallback, (void*) &img);
 
+	reduceNoise = maxContrast = useEdges = true;
+
+	/*
+	 * Search is the matrix that we search for in the current image
+	 */
+	Mat search;
+
+	/*
+	 * Simulate video stream
+	 */
 	while(running) {
 		/*
 		 * Reset the image so drawing a rectangle is animated
 		 */
 		backup.copyTo(img);
 
-		/*
-		 * Reduce noise first
-		 */
-		if(reduce_noise) {
-			medianBlur(img, img, 3);
-		}
-
-		/*
-		 * Equalize the histograms of each channel of the image
-		 */
-		if(max_contrast) {
-			maximize_contrast(img);
-		}
-
-		/*
-		 * For best results, compute edges after
-		 * noise reduction and contrast maximization.
-		 */
-		if(use_edges) {
-			cvtColor(img, img, CV_BGR2GRAY);
-			Canny(img, img, 80, 320, 3);
-		}
+	// if(useEdges) {
+		// }
 
 		/*
 		 * Show help LAST or it will be affected by noise reduction/edge detection
@@ -81,12 +71,12 @@ int main(int argc, char *argv[]) {
 			 * Edge map uses grayscale. 
 			 * Set the font color to white so the help menu is visible.
 			 */
-			Scalar color = (use_edges) ? Scalar(255, 255, 2525) : Scalar(0, 0, 255);
+			Scalar color = (useEdges) ? Scalar(255, 255, 2525) : Scalar(0, 0, 255);
 			/*
 			 * Create newlines by drawing 30 pixels lower
 			 */
 			for(int k = 0; k < HELP_LENGTH; k++) {
-				putText(img, help_array[k], Point(10, 30 + (k * 30)), 
+				putText(img, helpArray[k], Point(10, 30 + (k * 30)), 
 					FONT_HERSHEY_COMPLEX_SMALL, 0.8, color, 1, CV_AA);
 			}
 		}
@@ -95,7 +85,7 @@ int main(int argc, char *argv[]) {
 			/*
 			 * Draw the current mouse selection
 			 */
-			Scalar rect_color = (use_edges) ? Scalar(255, 255, 2525) : Scalar(0, 255, 0);
+			Scalar rect_color = (useEdges) ? Scalar(255, 255, 2525) : Scalar(0, 255, 0);
 			rectangle(img, selection, rect_color, 1, 8, 0);
 		}
 		else if(finished) {
@@ -106,9 +96,17 @@ int main(int argc, char *argv[]) {
 			 */
 			if(selection.width < 10 || selection.height < 10)
 				continue;
-		
-			Mat interest = img(selection);
-			imshow("Region of Interest", interest);
+
+			Mat subimage = img(selection);
+			Rect result;
+
+			// Try to find subimage in backup, and store the result in result
+			findSelection(backup, subimage, result);
+
+			if(result.width > 20 && result.height && 20) {
+				namedWindow("Region of Interest", CV_WINDOW_AUTOSIZE);
+				imshow("Region of Interest", backup(result));
+			}
 		}
 
 		// show the image in a window
@@ -138,30 +136,32 @@ int main(int argc, char *argv[]) {
 			 * Press e to toggle edge map
 			 */
 			case 'e': {
-				use_edges = !use_edges;
+				useEdges = !useEdges;
 				break;
 		  	}
 			/*
 			 * Press r to toggle noise reduction
 			 */
 			case 'r': {
-				reduce_noise = !reduce_noise;
+				reduceNoise = !reduceNoise;
 				break;
 			}
 			/*
 			 * Press c to toggle contrast maximization
 			 */
 			case 'c': {
-				max_contrast = !max_contrast;
+				maxContrast = !maxContrast;
 				break;
 			}
 		}
 	}
 
+	destroyAllWindows();
+
 	return 0;
 }
 
-void mouse_callback(int event, int x, int y, int flags, void* param) {
+void mouseCallback(int event, int x, int y, int flags, void* param) {
 	Mat* current_image = (Mat*) param;
 
 	switch(event) {
@@ -216,10 +216,119 @@ void mouse_callback(int event, int x, int y, int flags, void* param) {
  * Maximize the contrast by equalizing 
  * the histograms of BGR channels
  */
-void maximize_contrast(Mat &img) {
+void maximizeContrast(Mat &img) {
 	Mat channels[3];
 	split(img, channels);
 	for(int k = 0; k < 3; k++)
 		equalizeHist(channels[k], channels[k]);
 	merge(channels, 3, img);
+}
+
+/*
+ * Given a large image &img, find &subimage.
+ * Use the SURF algorithm to find unique points.
+ *
+ * For best results, compute edges after
+ * noise reduction and contrast maximization.
+ */
+void findSelection(Mat &img, Mat &subimage, Rect &result) {
+	Mat canvas;
+	img.copyTo(canvas);
+
+	maximizeContrast(img);
+	medianBlur(img, img, 3);
+
+	/*
+	 * Scene is the image
+	 * Object is the selection 
+	 */
+	Mat scene, object;
+	cvtColor(img, scene, CV_BGR2GRAY);
+	cvtColor(subimage, object, CV_BGR2GRAY);
+	Canny(scene, scene, 120, 480, 3);
+	Canny(object, object, 120, 480, 3);
+
+	/*
+	 * Calculate key points using SURF
+	 */
+	SurfFeatureDetector surf(400);
+	vector<KeyPoint> scene_points, object_points;
+	surf.detect(scene, scene_points);
+	surf.detect(object, object_points);
+
+	/*
+	 * Calculate descriptors
+	 */
+	SurfDescriptorExtractor extractor;
+	Mat scene_descriptor, object_descriptor;
+	extractor.compute(scene, scene_points, scene_descriptor);
+	extractor.compute(object, object_points, object_descriptor);
+
+	scene_descriptor.convertTo(scene_descriptor, CV_32F);
+	object_descriptor.convertTo(object_descriptor, CV_32F);
+
+	/*
+	 * Match the descriptor vectors using FLANN
+	 */
+	FlannBasedMatcher matcher;
+	vector<DMatch> matches;
+	matcher.match(scene_descriptor, object_descriptor, matches);
+
+	double max_dist = 0, min_dist = 50;
+
+	for(int i = 0; i < scene_descriptor.rows; i++) {
+		double dist = matches[i].distance;
+		if(dist < min_dist)
+			min_dist = dist;
+		if(dist> max_dist)
+			max_dist = dist;
+	}
+
+	/*
+	 * Find the matches that are less than 5 * min_dist apart
+	 */
+	vector<DMatch> good;
+	for(int i = 0; i < scene_descriptor.rows; i++) {
+		if(matches[i].distance < 2 * min_dist) {
+			good.push_back(matches[i]);
+		}
+	}
+
+	/*
+	 * Put the good matches into the scene and object vectors
+	 */
+	float curr_x, curr_y;
+	float max_x = 0, max_y = 0;
+	float min_x = scene.cols, min_y = scene.rows;
+
+	for(int i = 0; i < good.size(); i++) {
+		curr_x = scene_points[good[i].queryIdx].pt.x;
+		curr_y = scene_points[good[i].queryIdx].pt.y;
+		if(curr_x * curr_y != 0) {
+			max_x = (curr_x > max_x) ? curr_x : max_x;
+			max_y = (curr_y > max_y) ? curr_y : max_y;
+
+			min_x = (curr_x < min_x) ? curr_x : min_x;
+			min_y = (curr_y < min_y) ? curr_y : min_y;
+		}
+	}
+
+	cout << "Possible coordinates of match: [" << min_x << "," << min_y << "] to ["
+		 << max_x << "," << max_y << "]" << endl;
+
+	result.x = min_x;
+	result.y = min_y;
+	result.height = max_y - min_y;
+	result.width= max_x - min_x;
+
+	cout << "Image width: " << min_x + max_y << "\nImage height: " << min_y + max_y << endl;
+
+	Mat img_matches;
+
+	// for(int k = 0; k < good.size(); k++) {
+	// 	circle(canvas, scene_points[good[k].queryIdx].pt, 10, Scalar::all(255), 1, 8, 0);
+	// }
+
+	// namedWindow("Matches", CV_WINDOW_AUTOSIZE);
+	// imshow("Matches", canvas);
 }

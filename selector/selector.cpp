10ebc5368752e2b0365e75c7ddb131d97c020824
Brian Lee <brian@the-lee-home.us>
2013-03-26 13:11:03 -0500

    Add opencv_nonfree to cmake file

--- selector/selector.cpp
+++ selector/selector.cpp
@@ -19,6 +19,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/legacy/legacy.hpp>
 using namespace cv;
 
 #include <iostream>
